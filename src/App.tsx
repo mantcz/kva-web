d6d344507095ef61683f63ae7a00c09e2332d1ba
Michael Antczak <michael@sideslip.io>
2024-11-16 16:09:48 +0000

    chore: update app

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,15 @@ function App() {
   const [poses, setPoses] = useState<poseDetection.Pose[]>([]);
   // Add canvas ref
   const [canvasRef, setCanvasRef] = useState<HTMLCanvasElement | null>(null);
-  const [intervalId, setIntervalId] = useState<any | null>(null);
   // Add detector state
   const [detector, setDetector] = useState<poseDetection.PoseDetector | null>(
     null
   );
 
-  const MIN_CONFIDENCE = 0.5;
+  const MIN_CONFIDENCE = 0.4;
   const RADIUS = 5;
-  const WIDTH = 500;
-  const HEIGHT = 300;
-
-  // Add this state for animation frame tracking
-  const [animationFrameId, setAnimationFrameId] = useState<number | null>(null);
+  const WIDTH = 1000;
+  const HEIGHT = 700;
 
   // Add state to track if detection is running
   const [isRunning, setIsRunning] = useState(false);
@@ -73,6 +69,7 @@ function App() {
 
   // Add function to draw nose
   const draw = (poses: poseDetection.Pose[]) => {
+    // console.log("poses", poses);
     if (!canvasRef || !videoRef) return;
 
     const ctx = canvasRef.getContext("2d");
@@ -81,14 +78,17 @@ function App() {
     // Clear previous frame
     ctx.clearRect(0, 0, canvasRef.width, canvasRef.height);
 
-    // Calculate scale factors
     const scaleX = canvasRef.width / videoRef.videoWidth;
     const scaleY = canvasRef.height / videoRef.videoHeight;
 
-    const nose = poses[0].keypoints.find(
-      (point) =>
-        point.name === "nose" && point.score && point.score > MIN_CONFIDENCE
-    );
+    const nose = poses[0].keypoints.find((point) => point.name === "nose");
+    // const leftEar = poses[0].keypoints.find(
+    //   (point) => point.name === "left_ear"
+    // );
+    // const rightEar = poses[0].keypoints.find(
+    //   (point) => point.name === "right_ear"
+    // );
+
     const leftEye = poses[0].keypoints.find(
       (point) =>
         point.name === "left_eye" && point.score && point.score > MIN_CONFIDENCE
@@ -103,6 +103,17 @@ function App() {
     // const rightEar = poses[0].keypoints.find(
     //   (point) => point.name === "right_ear"
     // );
+
+    // Draw ear-nose-ear lines
+    if (nose && leftEye && rightEye) {
+      ctx.beginPath();
+      ctx.moveTo(leftEye.x * scaleX, leftEye.y * scaleY);
+      ctx.lineTo(nose.x * scaleX, nose.y * scaleY);
+      ctx.lineTo(rightEye.x * scaleX, rightEye.y * scaleY);
+      ctx.strokeStyle = "white";
+      ctx.lineWidth = 2;
+      ctx.stroke();
+    }
 
     const leftShoulder = poses[0].keypoints.find(
       (point) =>
@@ -127,6 +138,56 @@ function App() {
     const rightWrist = poses[0].keypoints.find(
       (point) =>
         point.name === "right_wrist" &&
+        point.score &&
+        point.score > MIN_CONFIDENCE
+    );
+
+    const leftElbow = poses[0].keypoints.find(
+      (point) =>
+        point.name === "left_elbow" &&
+        point.score &&
+        point.score > MIN_CONFIDENCE
+    );
+    const rightElbow = poses[0].keypoints.find(
+      (point) =>
+        point.name === "right_elbow" &&
+        point.score &&
+        point.score > MIN_CONFIDENCE
+    );
+
+    const leftHip = poses[0].keypoints.find(
+      (point) =>
+        point.name === "left_hip" && point.score && point.score > MIN_CONFIDENCE
+    );
+    const rightHip = poses[0].keypoints.find(
+      (point) =>
+        point.name === "right_hip" &&
+        point.score &&
+        point.score > MIN_CONFIDENCE
+    );
+
+    const leftKnee = poses[0].keypoints.find(
+      (point) =>
+        point.name === "left_knee" &&
+        point.score &&
+        point.score > MIN_CONFIDENCE
+    );
+    const rightKnee = poses[0].keypoints.find(
+      (point) =>
+        point.name === "right_knee" &&
+        point.score &&
+        point.score > MIN_CONFIDENCE
+    );
+
+    const leftAnkle = poses[0].keypoints.find(
+      (point) =>
+        point.name === "left_ankle" &&
+        point.score &&
+        point.score > MIN_CONFIDENCE
+    );
+    const rightAnkle = poses[0].keypoints.find(
+      (point) =>
+        point.name === "right_ankle" &&
         point.score &&
         point.score > MIN_CONFIDENCE
     );
@@ -212,6 +273,109 @@ function App() {
       ctx.fillStyle = "green";
       ctx.fill();
     }
+
+    if (leftElbow) {
+      ctx.beginPath();
+      ctx.arc(
+        leftElbow.x * scaleX,
+        leftElbow.y * scaleY,
+        RADIUS,
+        0,
+        2 * Math.PI
+      );
+      ctx.fillStyle = "white";
+      ctx.fill();
+    }
+    if (rightElbow) {
+      ctx.beginPath();
+      ctx.arc(
+        rightElbow.x * scaleX,
+        rightElbow.y * scaleY,
+        RADIUS,
+        0,
+        2 * Math.PI
+      );
+      ctx.fillStyle = "white";
+      ctx.fill();
+    }
+
+    // Draw wrist-elbow-arm lines
+    if (leftShoulder && leftElbow && leftWrist) {
+      ctx.beginPath();
+      ctx.moveTo(leftShoulder.x * scaleX, leftShoulder.y * scaleY);
+      ctx.lineTo(leftElbow.x * scaleX, leftElbow.y * scaleY);
+      ctx.lineTo(leftWrist.x * scaleX, leftWrist.y * scaleY);
+      ctx.strokeStyle = "white";
+      ctx.lineWidth = 2;
+      ctx.stroke();
+    }
+    if (rightShoulder && rightElbow && rightWrist) {
+      ctx.beginPath();
+      ctx.moveTo(rightShoulder.x * scaleX, rightShoulder.y * scaleY);
+      ctx.lineTo(rightElbow.x * scaleX, rightElbow.y * scaleY);
+      ctx.lineTo(rightWrist.x * scaleX, rightWrist.y * scaleY);
+      ctx.strokeStyle = "white";
+      ctx.lineWidth = 2;
+      ctx.stroke();
+    }
+
+    // Draw hip-hip lines
+    if (leftHip && rightHip) {
+      ctx.beginPath();
+      ctx.moveTo(leftHip.x * scaleX, leftHip.y * scaleY);
+      ctx.lineTo(rightHip.x * scaleX, rightHip.y * scaleY);
+      ctx.strokeStyle = "white";
+      ctx.lineWidth = 2;
+      ctx.stroke();
+    }
+
+    // Draw shoulder-shoulder lines
+    if (leftShoulder && rightShoulder) {
+      ctx.beginPath();
+      ctx.moveTo(leftShoulder.x * scaleX, leftShoulder.y * scaleY);
+      ctx.lineTo(rightShoulder.x * scaleX, rightShoulder.y * scaleY);
+      ctx.strokeStyle = "white";
+      ctx.lineWidth = 2;
+      ctx.stroke();
+    }
+
+    // Draw ankle-knee-hip lines
+    if (leftHip && leftKnee && leftAnkle) {
+      ctx.beginPath();
+      ctx.moveTo(leftHip.x * scaleX, leftHip.y * scaleY);
+      ctx.lineTo(leftKnee.x * scaleX, leftKnee.y * scaleY);
+      ctx.lineTo(leftAnkle.x * scaleX, leftAnkle.y * scaleY);
+      ctx.strokeStyle = "white";
+      ctx.lineWidth = 2;
+      ctx.stroke();
+    }
+
+    if (rightHip && rightKnee && rightAnkle) {
+      ctx.beginPath();
+      ctx.moveTo(rightHip.x * scaleX, rightHip.y * scaleY);
+      ctx.lineTo(rightKnee.x * scaleX, rightKnee.y * scaleY);
+      ctx.lineTo(rightAnkle.x * scaleX, rightAnkle.y * scaleY);
+      ctx.strokeStyle = "white";
+      ctx.lineWidth = 2;
+      ctx.stroke();
+    }
+
+    // Draw mid-hips to mid-shoulders
+    if (leftHip && rightHip && leftShoulder && rightShoulder) {
+      // Calculate midpoints
+      const midHipX = (leftHip.x + rightHip.x) / 2;
+      const midHipY = (leftHip.y + rightHip.y) / 2;
+      const midShoulderX = (leftShoulder.x + rightShoulder.x) / 2;
+      const midShoulderY = (leftShoulder.y + rightShoulder.y) / 2;
+
+      // Draw line between midpoints
+      ctx.beginPath();
+      ctx.moveTo(midHipX * scaleX, midHipY * scaleY);
+      ctx.lineTo(midShoulderX * scaleX, midShoulderY * scaleY);
+      ctx.strokeStyle = "white";
+      ctx.lineWidth = 2;
+      ctx.stroke();
+    }
   };
 
   // Modify detect function
@@ -255,7 +419,10 @@ function App() {
             // border: "3px solid red",
           }}
         ></canvas>
-        <video ref={setVideoRef} style={{ width: "500px", height: "300px" }} />
+        <video
+          ref={setVideoRef}
+          style={{ width: `${WIDTH}px`, height: `${HEIGHT}px` }}
+        />
       </div>
       <div
         style={{
@@ -274,14 +441,14 @@ function App() {
         >
           Start
         </button>
-        <button
+        {/* <button
           onClick={() => {
             setIsRunning(false);
           }}
           style={{ backgroundColor: "red", color: "white" }}
         >
           Stop
-        </button>
+        </button> */}
       </div>
     </div>
   );
